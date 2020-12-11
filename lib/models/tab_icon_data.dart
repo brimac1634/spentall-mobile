a558a137ff6ec6cd8dfa04b86c10300a7eb5e697
Brian MacPherson <brimac1634@gmail.com>
2020-12-11 12:53:03 +0800

    changed styling of other tabs

--- lib/models/tab_icon_data.dart
+++ lib/models/tab_icon_data.dart
@@ -19,28 +19,28 @@ class TabIconData {
   static List<TabIconData> tabIconsList = <TabIconData>[
     TabIconData(
       imagePath: 'assets/icons/home.png',
-      selectedImagePath: 'assets/icons/home.png',
+      selectedImagePath: 'assets/icons/homeS.png',
       index: 0,
       isSelected: true,
       animationController: null,
     ),
     TabIconData(
       imagePath: 'assets/icons/list.png',
-      selectedImagePath: 'assets/icons/list.png',
+      selectedImagePath: 'assets/icons/listS.png',
       index: 1,
       isSelected: false,
       animationController: null,
     ),
     TabIconData(
       imagePath: 'assets/icons/analytics.png',
-      selectedImagePath: 'assets/icons/analytics.png',
+      selectedImagePath: 'assets/icons/analyticsS.png',
       index: 2,
       isSelected: false,
       animationController: null,
     ),
     TabIconData(
       imagePath: 'assets/icons/settings.png',
-      selectedImagePath: 'assets/icons/settings.png',
+      selectedImagePath: 'assets/icons/settingsS.png',
       index: 3,
       isSelected: false,
       animationController: null,
