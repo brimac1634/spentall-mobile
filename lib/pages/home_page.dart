a9e82ed195f5d18ea5f0b9edc5af82ca616e7c73
Brian MacPherson <brimac1634@gmail.com>
2020-12-14 19:06:39 +0800

    added animation to home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -17,41 +17,62 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
+  Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _animation = Tween<double>(begin: 0.0, end: 1.0).animate(CurvedAnimation(
+        parent: widget.animationController, curve: Curves.fastOutSlowIn));
+
+    widget.animationController.forward();
+  }
+
   @override
   Widget build(BuildContext context) {
     final expenses = Provider.of<Expenses>(context);
     final auth = Provider.of<Auth>(context);
+
     return RefreshIndicator(
-      onRefresh: () async {
-        await expenses.getExpenses();
-      },
-      child: SingleChildScrollView(
-        physics: AlwaysScrollableScrollPhysics(),
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                height: 30,
+        onRefresh: () async {
+          await expenses.getExpenses();
+        },
+        child: SingleChildScrollView(
+            physics: AlwaysScrollableScrollPhysics(),
+            child: AnimatedBuilder(
+              animation: widget.animationController,
+              builder: (context, _) => FadeTransition(
+                opacity: _animation,
+                child: Transform(
+                  transform: Matrix4.translationValues(
+                      0.0, 40 * (1.0 - _animation.value), 0.0),
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                          height: 30,
+                        ),
+                        PercentMeter(
+                            expenses.cycleTotalTargetPercentage.ceil()),
+                        Container(
+                          height: 20,
+                        ),
+                        Text(
+                          '${expenses.cycleTotalTargetPercentage.ceil()}% left of your ${auth.user.cycle} limit',
+                          style: Theme.of(context).textTheme.headline2,
+                        ),
+                        Text(
+                          'or',
+                          style: Theme.of(context).textTheme.headline2,
+                        ),
+                        Text(
+                          '\$${utils.formatAmount(expenses.cycleFilteredTotalExpenses)} out of \$${auth.user.target}',
+                          style: Theme.of(context).textTheme.headline2,
+                        ),
+                      ]),
+                ),
               ),
-              PercentMeter(expenses.cycleTotalTargetPercentage.ceil()),
-              Container(
-                height: 20,
-              ),
-              Text(
-                '${expenses.cycleTotalTargetPercentage.ceil()}% left of your ${auth.user.cycle} limit',
-                style: Theme.of(context).textTheme.headline2,
-              ),
-              Text(
-                'or',
-                style: Theme.of(context).textTheme.headline2,
-              ),
-              Text(
-                '\$${utils.formatAmount(expenses.cycleFilteredTotalExpenses)} out of \$${auth.user.target}',
-                style: Theme.of(context).textTheme.headline2,
-              ),
-            ]),
-      ),
-    );
+            )));
   }
 }
