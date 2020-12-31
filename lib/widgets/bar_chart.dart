c45f4c611034dd71e199b044ce4366ab85543966
Brian MacPherson <brimac1634@gmail.com>
2020-12-31 16:58:34 +0800

    updated filter bar and loading indicators, tilted labels in bar chart

--- lib/widgets/bar_chart.dart
+++ lib/widgets/bar_chart.dart
@@ -3,6 +3,8 @@ import 'package:charts_flutter/flutter.dart' as charts;
 
 import '../models/bar_data.dart';
 
+import '../app_theme.dart';
+
 class BarChart extends StatelessWidget {
   final List<BarData> data;
 
@@ -12,7 +14,7 @@ class BarChart extends StatelessWidget {
     return [
       charts.Series<BarData, String>(
         id: 'Time Data',
-        colorFn: (_, __) => charts.MaterialPalette.blue.shadeDefault,
+        colorFn: (_, __) => charts.ColorUtil.fromDartColor(AppTheme.pink),
         domainFn: (BarData bar, _) => bar.label,
         measureFn: (BarData bar, _) => bar.amount,
         data: data,
@@ -25,6 +27,9 @@ class BarChart extends StatelessWidget {
     return new charts.BarChart(
       _setBarData(data),
       animate: true,
+      domainAxis: charts.OrdinalAxisSpec(
+        renderSpec: charts.SmallTickRendererSpec(labelRotation: 60),
+      ),
     );
   }
 }
--- lib/widgets/filter_bar.dart
+++ lib/widgets/filter_bar.dart
@@ -22,7 +22,6 @@ class FilterBar extends StatefulWidget {
 }
 
 class _FilterBarState extends State<FilterBar> {
-  static const _dateFilterFormat = 'd MMM yyyy';
   static const _quickTimeFilters = [
     'today',
     'this week',
@@ -32,6 +31,8 @@ class _FilterBarState extends State<FilterBar> {
 
   var _showSearch = false;
   var _isLoading = false;
+
+  var _isLoadingDateChange = false;
 
   void _deleteSelectedExpenses(Expenses expenses) async {
     setState(() {
@@ -56,12 +57,24 @@ class _FilterBarState extends State<FilterBar> {
     if (dateRange.isWithinRange(expenses.cycleDateRange)) {
       expenses.setTimeFilter(dateRange);
     } else {
-      expenses.setTimeFilter(dateRange);
-      await expenses.getExpenses(queryType: ExpenseQuery.dateRange);
+      setState(() {
+        _isLoadingDateChange = true;
+      });
+
+      try {
+        expenses.setTimeFilter(dateRange);
+        await expenses.getExpenses(queryType: ExpenseQuery.dateRange);
+      } catch (err) {
+        print(err);
+      } finally {
+        setState(() {
+          _isLoadingDateChange = false;
+        });
+      }
     }
   }
 
-  void _presentFiltersAndSorters() {
+  void _presentSorters() {
     showDialog(
       context: context,
       builder: (context) {
@@ -70,47 +83,6 @@ class _FilterBarState extends State<FilterBar> {
             child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 12),
           child: Column(children: [
-            Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: Text(
-                'Date Range',
-                style: AppTheme.headline5,
-              ),
-            ),
-            Padding(
-                padding: const EdgeInsets.symmetric(vertical: 12),
-                child: InkWell(
-                  splashColor: Colors.transparent,
-                  onTap: () {
-                    _presentDatePicker(_expenses);
-                  },
-                  child: Container(
-                    padding: const EdgeInsets.all(12),
-                    decoration: BoxDecoration(
-                        color: AppTheme.lightPurple,
-                        borderRadius: BorderRadius.circular(25),
-                        boxShadow: [
-                          BoxShadow(
-                            color: AppTheme.darkerPurple,
-                            offset: Offset(0.0, 1.5),
-                            blurRadius: 1.5,
-                          ),
-                        ]),
-                    child: Row(mainAxisSize: MainAxisSize.min, children: [
-                      Text(
-                        utils.formatDateRange(_expenses.filterRange),
-                        style: AppTheme.label2,
-                      ),
-                      SizedBox(
-                        width: 8,
-                      ),
-                      Icon(
-                        Icons.calendar_today_outlined,
-                        color: AppTheme.offWhite,
-                      ),
-                    ]),
-                  ),
-                )),
             Padding(
               padding: const EdgeInsets.all(12.0),
               child: Text(
@@ -189,60 +161,78 @@ class _FilterBarState extends State<FilterBar> {
     );
   }
 
-  void _presentDatePicker(Expenses expenses) {
-    showDialog(
-      context: context,
-      builder: (context) {
-        return CustomDialog(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 12),
-            child: Column(children: [
+  Future<void> _presentDatePicker(Expenses expenses) async {
+    try {
+      final _dateRange = await showDialog(
+        context: context,
+        builder: (context) {
+          return CustomDialog(
+            child: Stack(children: [
               Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: Text(
-                  'Time Filter',
-                  style: AppTheme.headline5,
-                ),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 8.0, vertical: 12),
+                child: Column(children: [
+                  Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Text(
+                      'Time Filter',
+                      style: AppTheme.headline5,
+                    ),
+                  ),
+                  Wrap(
+                    direction: Axis.horizontal,
+                    alignment: WrapAlignment.center,
+                    children: _quickTimeFilters
+                        .map((filter) => Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: InkWell(
+                                splashColor: Colors.transparent,
+                                onTap: () {
+                                  final dateRange = utils.getCycleDates(filter);
+                                  Navigator.of(context).pop(dateRange);
+                                },
+                                child: Chip(
+                                    backgroundColor: AppTheme.lightPurple,
+                                    label: Padding(
+                                      padding: const EdgeInsets.symmetric(
+                                          vertical: 8.0, horizontal: 12),
+                                      child: Text(filter.capitalize(),
+                                          style: AppTheme.label2),
+                                    )),
+                              ),
+                            ))
+                        .toList(),
+                  ),
+                  Calendar(
+                    startDate: expenses.filterRange.start,
+                    endDate: expenses.filterRange.end,
+                    range: true,
+                    onSelectRange: (dateRange) {
+                      Navigator.of(context).pop(dateRange);
+                    },
+                  ),
+                ]),
               ),
-              Wrap(
-                direction: Axis.horizontal,
-                alignment: WrapAlignment.center,
-                children: _quickTimeFilters
-                    .map((filter) => Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: InkWell(
-                            splashColor: Colors.transparent,
-                            onTap: () async {
-                              final dateRange = utils.getCycleDates(filter);
-                              await _updateTimeFilter(expenses, dateRange);
-                              Navigator.of(context).pop();
-                            },
-                            child: Chip(
-                                backgroundColor: AppTheme.lightPurple,
-                                label: Padding(
-                                  padding: const EdgeInsets.symmetric(
-                                      vertical: 8.0, horizontal: 12),
-                                  child: Text(filter.capitalize(),
-                                      style: AppTheme.label2),
-                                )),
-                          ),
-                        ))
-                    .toList(),
-              ),
-              Calendar(
-                startDate: expenses.filterRange.start,
-                endDate: expenses.filterRange.end,
-                range: true,
-                onSelectRange: (dateRange) async {
-                  _updateTimeFilter(expenses, dateRange);
-                  Navigator.of(context).pop();
-                },
-              ),
+              if (_isLoadingDateChange)
+                Center(child: CircularProgressIndicator())
             ]),
-          ),
-        );
-      },
-    );
+          );
+        },
+      );
+      if (_dateRange == null) return;
+
+      setState(() {
+        _isLoadingDateChange = true;
+      });
+
+      await _updateTimeFilter(expenses, _dateRange);
+    } catch (err) {
+      print(err);
+    } finally {
+      setState(() {
+        _isLoadingDateChange = false;
+      });
+    }
   }
 
   @override
@@ -270,19 +260,32 @@ class _FilterBarState extends State<FilterBar> {
                   mainAxisAlignment: MainAxisAlignment.end,
                   children: <Widget>[
                     ScaleIconButton(
-                      imageAsset: 'assets/icons/filter.png',
-                      onTap: () {
-                        // _presentDatePicker(_expenseData);
-                        _presentFiltersAndSorters();
-                      },
+                      imageAsset: 'assets/icons/sort.png',
+                      onTap: _presentSorters,
                     ),
                     SizedBox(
-                      width: 16,
+                      width: 12,
+                    ),
+                    _isLoadingDateChange
+                        ? SizedBox(
+                            width: 32,
+                            height: 32,
+                            child: CircularProgressIndicator(
+                              valueColor: AlwaysStoppedAnimation<Color>(
+                                  AppTheme.darkPurple),
+                            ),
+                          )
+                        : ScaleIconButton(
+                            imageAsset: 'assets/icons/calendar.png',
+                            onTap: () {
+                              _presentDatePicker(_expenseData);
+                            },
+                          ),
+                    SizedBox(
+                      width: 12,
                     ),
                     ScaleIconButton(
-                      imageAsset: _showSearch
-                          ? 'assets/icons/searchS.png'
-                          : 'assets/icons/search.png',
+                      imageAsset: 'assets/icons/search.png',
                       onTap: () {
                         setState(() {
                           _showSearch = !_showSearch;
@@ -336,8 +339,13 @@ class _FilterBarState extends State<FilterBar> {
                     ),
                   ),
                   _isLoading
-                      ? CircularProgressIndicator(
-                          backgroundColor: AppTheme.darkPurple,
+                      ? SizedBox(
+                          width: 32,
+                          height: 32,
+                          child: CircularProgressIndicator(
+                            valueColor:
+                                AlwaysStoppedAnimation<Color>(Colors.red),
+                          ),
                         )
                       : InkWell(
                           splashColor: Colors.transparent,
